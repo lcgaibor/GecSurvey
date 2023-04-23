9d1aa2612a43e9e3608e910ada91da20313b3549
lcgaibor <lcgaibor@espe.edu.ec>
2023-04-23 11:38:08 -0500

    perfil elementos

--- app/src/main/java/com/example/myapplication/Perfil.java
+++ app/src/main/java/com/example/myapplication/Perfil.java
@@ -3,12 +3,32 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Perfil extends AppCompatActivity {
+
+    TextView correo, nombre, apellido, contrasenia;
+    Button btnEditar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.perfil);
+
+        correo = findViewById(R.id.textView18);
+        nombre = findViewById(R.id.textView549);
+        apellido = findViewById(R.id.textView449);
+        contrasenia = findViewById(R.id.textView49);
+
+        btnEditar = findViewById(R.id.button554);
+
+        btnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
